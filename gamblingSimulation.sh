209a91bb492a818554afe09153de54e1c77f1bb8
arun kumar <alekhya0801@gmail.com>
2020-04-12 16:28:13 +0530

    Added feature to compute total profit or loss amount for a month

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -7,13 +7,24 @@ MAX_PROFIT_PER_DAY=50
 MAX_LOSS_PER_DAY=-50
 #variables
 earnMoney=0
-while [ $earnMoney -gt $MAX_LOSS_PER_DAY -a $earnMoney -lt $MAX_PROFIT_PER_DAY ]
+totalAmount=0
+for (( i=1; i<=20; i++))
 do
-	if [ $WIN -eq $((RANDOM%2)) ]
-	then
-		earnMoney=$(($earnMoney+$BET))
-	else
-		earnMoney=$(($earnMoney-$BET))
-	fi
+	earnMoney=0
+	while [ $earnMoney -gt $MAX_LOSS_PER_DAY -a $earnMoney -lt $MAX_PROFIT_PER_DAY ]
+	do
+		if [ $WIN -eq $((RANDOM%2)) ]
+		then
+			earnMoney=$(($earnMoney+$BET))
+		else
+			earnMoney=$(($earnMoney-$BET))
+		fi
+	done
+	totalAmount=$(($totalAmount+$earnMoney))
 done
-echo $earnMoney
+if [ $totalAmount -gt 0 ]
+then
+	echo $totalAmount dollar won
+else
+	echo $totalAmount dollar loss
+fi
