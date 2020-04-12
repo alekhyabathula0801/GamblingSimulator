e9d66085e8e12bfccb9dbd9f65b7e00846b2c365
arun kumar <alekhya0801@gmail.com>
2020-04-12 19:16:40 +0530

    Added feature to know days won and lost

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -7,8 +7,9 @@ MAX_PROFIT_PER_DAY=50
 MAX_LOSS_PER_DAY=-50
 #variables
 earnMoney=0
-totalAmount=0
-for (( i=1; i<=20; i++))
+numOfWins=0
+numOfLosses=0
+for (( day=1; day<=20; day++))
 do
 	earnMoney=0
 	while [ $earnMoney -gt $MAX_LOSS_PER_DAY -a $earnMoney -lt $MAX_PROFIT_PER_DAY ]
@@ -20,11 +21,21 @@ do
 			earnMoney=$(($earnMoney-$BET))
 		fi
 	done
-	totalAmount=$(($totalAmount+$earnMoney))
+	if [ $earnMoney -eq 50 ]
+	then
+		win[$day]=50
+		((numOfWins++))
+	else
+		loose[$day]=-50
+		((numOfLosses++))
+	fi
 done
+totalAmount=$((50*(($numOfWins-$numOfLosses))))
 if [ $totalAmount -gt 0 ]
 then
 	echo $totalAmount dollar won
 else
 	echo $totalAmount dollar loss
 fi
+echo On ${!win[@]} days he won $((50*$numOfWins)) dollar
+echo On ${!loose[@]} days he lost $((50*$numOfLosses)) dollar
