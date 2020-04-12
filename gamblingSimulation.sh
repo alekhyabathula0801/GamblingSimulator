ea473a34167ae47204b37cf14a71a85379fbd5f9
arun kumar <alekhya0801@gmail.com>
2020-04-12 15:31:54 +0530

    Added condition to resign for the day feature

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,9 +3,17 @@
 BET=1
 STAKE=100
 WIN=1
-if [ $WIN -eq $((RANDOM%2)) ]
-then
-	echo won 1 dollar
-else
-	echo loose 1 dollar
-fi
+MAX_PROFIT_PER_DAY=50
+MAX_LOSS_PER_DAY=-50
+#variables
+earnMoney=0
+while [ $earnMoney -gt $MAX_LOSS_PER_DAY -a $earnMoney -lt $MAX_PROFIT_PER_DAY ]
+do
+	if [ $WIN -eq $((RANDOM%2)) ]
+	then
+		earnMoney=$(($earnMoney+$BET))
+	else
+		earnMoney=$(($earnMoney-$BET))
+	fi
+done
+echo $earnMoney
